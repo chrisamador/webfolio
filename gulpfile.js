001eaeefe88b551358e7210a518260bbb4a173bb
Chris Amador Mac Air <chris@chrisamador.me>
2016-06-28 13:49:19 -0400

    Gulpfile.js update

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,13 @@ var gulp = require('gulp'),
    concat = require('gulp-concat'),
    jshint = require('gulp-jshint'),
    uglify = require('gulp-uglify'),
-   bowerFiles = require('gulp-main-bower-files');
+   mainBowerFiles = require('gulp-main-bower-files'),
+   open = require('gulp-open'),
+   filter = require('gulp-filter');
 
 config ={
-	src : "./src",
-	dist : './dist'
+	src : 'src',
+	dest : 'public'
 }
 
 /**
@@ -26,7 +28,10 @@ config ={
  *
  */
 
-gulp.task('default', ['styles','bowerJs','bowerCss','server','watch']);
+gulp.task('default', ['html','styles', 'js' ,'bowerJs','bowerLess','server','watch'], function(){
+	gulp.src(__filename)
+  .pipe(open({uri: 'http://localhost:4000'}));
+});
 
 /**
  *
@@ -36,7 +41,7 @@ gulp.task('default', ['styles','bowerJs','bowerCss','server','watch']);
 
 gulp.task('server',function(){
 	var app = express();
-	app.use(express.static(config.dist));
+	app.use(express.static(config.dest));
 	app.listen(4000);
 });
 
@@ -49,47 +54,63 @@ gulp.task('server',function(){
 gulp.task('watch', function(){
 	livereload.listen();
 
-	gulp.watch('src/js/*.js',['js']);
-	gulp.watch('src/*.html',['html']);
-	gulp.watch('src/less/*.less',['styles']);
+	gulp.watch(config.src + '/js/*/**.js',['js']);
+	gulp.watch(config.src + '/**/*.html',['html']);
+	gulp.watch(config.src + '/less/*/**.less',['styles']);
 })
 
 
 gulp.task('bowerJs', function(){
-	return gulp.src(bowerFiles())
-			.pipe(filter('*.js'))
-			.pipe(concat('vendor.js'))
+	return gulp.src('./bower.json')
+			.pipe(mainBowerFiles())
+			.pipe(filter('**/*.js'))
+			.pipe(concat('vendor.min.js'))
 			.pipe(uglify())
-			.pipe(gulp.dest(config.dist + '/js'));
+			.pipe(gulp.dest(config.dest + '/js'));
+
 });
 
 gulp.task('js', function(){
-	return gulp.src('src/js/*.js')
-			.pipe(gulp.dest(config.dist + '/js'))
+	return gulp.src(config.src + '/js/*.js')
+			.pipe(gulp.dest(config.dest + '/js'))
 			.pipe(livereload());
 });
 
 gulp.task('html', function(){
-	return gulp.src('src/*.html')
+	return gulp.src(config.src + '/**/*.html')
+			.pipe(gulp.dest(config.dest + '/'))
 			.pipe(livereload());
 });
 
-gulp.task('bowerCss', function(){
-	return gulp.src(plugins.bowerFiles())
-			.pipe(plugins.filter('*.css'))
-			.pipe(plugins.concat('lib.css'))
-			.pipe(plugins.uglify())
-			.pipe(gulp.dest(config.dist + '/css'));
+gulp.task('bowerLess', function(){
+		gulp.src('./bower.json')
+		.pipe(mainBowerFiles())
+		.pipe(filter('**/*.less'))
+		.pipe(less())
+		.pipe(minifycss())
+		.pipe(concat('lib.min.css'))
+		.pipe(gulp.dest(config.dest + '/css'))
+
+	return gulp.src('./bower.json')
+		.pipe(mainBowerFiles())
+		.pipe(filter('**/*.less'))
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(sourcemaps.write())
+		.pipe(concat('lib.css'))
+		.pipe(gulp.dest(config.dest + '/css'));
 });
 
 gulp.task('styles', function() {
-  return gulp.src('src/less/main.less')
+  return gulp.src(config.src + '/less/app.less')
+			.pipe(sourcemaps.init())
 			.pipe(less())
+			.pipe(sourcemaps.write())
 			.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-			.pipe(gulp.dest('src/css'))
+			.pipe(gulp.dest(config.src + '/css'))
 			.pipe(rename({ suffix: '.min' }))
 			.pipe(minifycss())
-			.pipe(gulp.dest(config.dist + '/css'))
+			.pipe(gulp.dest(config.dest + '/css'))
 			.pipe(livereload());
 });
 
