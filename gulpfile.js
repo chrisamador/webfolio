b2185d16e1dc27d7a441dc53720d6d2fc5276cd1
Chris Amador - Macbook Pro <chris@chrisamador.me>
2016-08-09 14:02:20 -0400

    Progress

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('watch', function(){
 	livereload.listen();
 
 	gulp.watch(config.src + '/js/**/*.js',['js']);
-	gulp.watch(config.src + '/**/*.html',['html']);
+	gulp.watch(config.src + '/**/*.+(html|nunjucks)',['html']);
 	gulp.watch(config.src + '/pages/works/works.json',['html']);
 	gulp.watch(config.src + '/less/**/*.less',['styles']);
 })
