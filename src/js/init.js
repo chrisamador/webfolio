889676aaf0a378226cfeb1fc33149870ac903923
Chris Amador - Macbook Pro <chris@chrisamador.me>
2016-08-08 20:31:25 -0400

    Progress

--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,26 @@
 define(['jquery'], function ($) {
-
-
   return function initRun() {
     console.log('Init Run!!');
-  }
 
+
+    var $header = $(document.getElementById('header')),
+        $window = $(window),
+        $mobileBtn = $(document.getElementById('mobile-btn'));
+
+    function headerScroll(e) {
+      if($window.scrollTop() > 40){
+        $header.addClass('scrolled-view');
+      }else{
+        $header.removeClass('scrolled-view');
+      }
+    };
+
+    $(window).on('load scroll mousewheel touchMove',headerScroll)
+
+    $mobileBtn.on('click', function $mobileBtnClick(e) {
+      e.preventDefault();
+      alert('cool');
+    });
+
+  }
 });
